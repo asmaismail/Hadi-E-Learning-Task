3a1bd4a9412b293ebfb8fecb454e51731b3590fd
asmaismail <asmaismail202020@gmail.com>
2024-04-27 22:53:18 +0500

    upload

--- vite dashboard/src/App.jsx
+++ vite dashboard/src/App.jsx
@@ -3,9 +3,14 @@ import Appbar from './Components/Appbar'
 
 const App = () => {
   return (
-    <div className='bg-red-600 w-full h-14'>App
+    <>
+    <BrowserRouter>
+    <Routes>
+      
+      </Routes>
+    </BrowserRouter>
       <Appbar/>
-    </div>
+    </>
   )
 }
 
